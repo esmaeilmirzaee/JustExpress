f047b2f1436bb7155f085c2599b9b08f16c066a0
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2021-05-03 20:35:39 +0430

    Login a user

--- express301/index.js
+++ express301/index.js
@@ -2,31 +2,62 @@ const express = require('express');
 const app = express();
 
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 let port = process.env.PORT || 3000;
 app.use(helmet());
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+let justValid = false;
 
 app.set('view engine', 'ejs');
 app.set('views', `${process.cwd()}/views`);
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.render('home');
 });
 
-app.get('/login', (req, res) => {
+app.get('/login', (req, res, next) => {
     res.render('login');
 });
 
-app.post('/process_login', (req, res) => {
-    res.json({ message: 'Loggedin' });
+// session stored in the server and will send to user when it requested
+// cookies stored in the browser
+app.post('/process_login', (req, res, next) => {
+    console.log(req.body);
+    const { email, password } = req.body;
+    if (password === '123456789') {
+        justValid = true;
+        res.cookie('email', email);
+        res.redirect('/welcome');
+    } else {
+        res.redirect('/login');
+    }
 });
 
-app.get('*', (req, res) => {
+app.get('/welcome', validateUser, (req, res) => {
+    if (res.locals.validate) {
+        res.render('welcome', { email: req.cookies.email });
+    } else {
+        res.redirect('/login');
+    }
+});
+
+function validateUser(req, res, next) {
+    if (req.cookies.email) {
+        res.locals.validate = true;
+    } else {
+        res.locals.validate = false;
+    }
+    next();
+}
+
+// unwanted path
+app.get('*', (req, res, next) => {
     res.send('<div class="container"><h1>Not found</h1></div>');
 });
 
 app.listen(port, () => {
-    console.log(`App is listening on ${port}.`);
+    console.log(`Listening on ${port}.`);
 });
